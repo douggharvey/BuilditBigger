086fabadb8bb5ac7c9594a630f58b335985ebbee
douggharvey <douggharvey@gmail.com>
2018-04-02 16:32:45 +0300

    Initial commit

---
+++ JokeProvider/src/main/java/com/douglasharvey/jokeprovider/JokeTeller.java
@@ -0,0 +1,7 @@
+package com.douglasharvey.jokeprovider;
+
+public class JokeTeller {
+    public String tellJoke(){
+        return "this is a joke";
+    }
+}
---
+++ jokedisplay/src/main/java/com/douglasharvey/jokedisplay/JokeDisplayActivity.java
@@ -0,0 +1,36 @@
+package com.douglasharvey.jokedisplay;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class JokeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_display);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String joke=null;
+        if (getIntent().hasExtra("JOKE"))
+            joke = getIntent().getStringExtra("JOKE");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(joke);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
